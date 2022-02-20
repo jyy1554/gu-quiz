63de760cbe191c3988e4b8c73cd4c89a2a04e1c4
jyy1554 <jyy1556@gmail.com>
2022-02-20 12:26:25 +0900

    welcome 페이지에서 마우스오버 구현

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import Button from '@mui/material/Button';
+import MouseOverPopover from './components/MouseOverPopover';
 import './css/index.css';
 
 class Welcome extends Component {
@@ -9,7 +9,7 @@ class Welcome extends Component {
       <div className="welcome">
         <div class="wrapper">
           <span class="info-icon">
-            <InfoOutlinedIcon fontSize="large" htmlColor='lightgray' />
+            <MouseOverPopover />
           </span>
           <div class="title-container">
             <div class="main-title">구 이름 맞추기</div>
