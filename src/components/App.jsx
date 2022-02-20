63a1dae43228604f9f2777d746429268b0caa3eb
jyy1554 <jyy1556@gmail.com>
2022-02-20 13:33:13 +0900

    라우팅 기능 적용

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
@@ -7,11 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="layout-container">
-          <Header />
-          <Welcome />
-          <Footer />
-        </div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+        <Footer />
       </div>
     );
   }
