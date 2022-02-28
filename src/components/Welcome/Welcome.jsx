5fe7199cae0cbe27e34fb417051dce8bcfdb66aa
jyy1554 <jyy1556@gmail.com>
2022-02-28 21:42:59 +0900

    아이콘 및 페이지명 수정

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -21,7 +21,7 @@ function Welcome() {
         </span>
         <div className="title-container">
           <div className="main-title">구 이름 맞추기</div>
-          <div className="sub-title">누가누가 서울 고수인가?!</div>
+          <div className="sub-title">누가누가 찐 서울 고수인가?!</div>
           <div className="start-btn" onClick={__gameStart}>
             <button className='game-start'>게임시작</button>
           </div>
